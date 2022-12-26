01dd9e0ed1f47cab67cd8b7e3456097e93cce8f5
SpykerNZ <jrspyker@gmail.com>
2022-12-27 09:13:46 +1300

    finish basic UI

--- scripts.js
+++ scripts.js
@@ -4,7 +4,63 @@ const resultArray = ["Win", "Lose", "Draw"];
 const outcomeArray = ["Beats", "Draws"];
 
 function playRound(choice) {
-    result.textContent = fight(choice.textContent, getComputerChoice());
+    resultString.textContent = fight(choice.textContent, getComputerChoice());
+    updateScores();
+    checkWinner();
+}
+
+function updateScores() {
+    playerScoreValue.textContent = playerScore;
+    computerScoreValue.textContent = computerScore;
+}
+
+function checkWinner() {
+    if (computerScore>=maxScore) {
+        resultString.textContent = 'Computer Wins!';
+        computerScoreBox.style.borderColor = 'green';
+        computerScoreBox.style.borderWidth = '4px';
+        gameEnd();
+    }
+    else if (playerScore>=maxScore) {
+        resultString.textContent = 'Challenger Wins!'
+        playerScoreBox.style.borderColor = 'green';
+        playerScoreBox.style.borderWidth = '4px';
+        gameEnd();
+    }
+}
+
+function resetGame() {
+    playerScore = 0;
+    computerScore = 0;
+    updateScores();
+    gameStart();
+}
+
+function gameEnd() {
+    // show rematch button
+    rematchButton.style.display = 'block';
+    // deactivate buttons
+    choiceButtons.forEach(choice => {
+        choice.disabled = true;
+    });
+}
+
+function gameStart() {
+    // Set game state
+    gameActive = true;
+    // hide rematch button
+    rematchButton.style.display = 'none';
+    // set boxes to default color
+    computerScoreBox.style.borderColor = 'blue';
+    computerScoreBox.style.borderWidth = '2px';
+    playerScoreBox.style.borderColor = 'blue';
+    playerScoreBox.style.borderWidth = '2px';
+    // reset string
+    resultString.textContent = 'The showdown of a lifetime!';
+    // activate buttons
+    choiceButtons.forEach(choice => {
+        choice.disabled = false;
+    });
 }
 
 function getComputerChoice() {
@@ -45,18 +101,37 @@ function fight(playerChoice, computerChoice) {
                 };
         }
     }
+
+    if (result===resultArray[1]) {
+        computerScore += 1;
+    } else if (result===resultArray[0]) {
+        playerScore += 1;
+    }
+
     return `You ${result}! ${winner} ${outcome} ${loser}.`;
 }
 
-function addChoiceListener(e) {
+const choiceButtons = document.querySelectorAll('.choice');
+const rematchButton = document.querySelector('.rematch');
+const resultString = document.querySelector('.result');
+const playerScoreValue = document.querySelector('.player > .score-value');
+const computerScoreValue = document.querySelector('.computer > .score-value');
+const playerScoreBox = document.querySelector('.player.score-box');
+const computerScoreBox = document.querySelector('.computer.score-box');
 
-}
+const maxScore = 5;
+let computerScore = 0;
+let playerScore = 0;
+let gameActive = 1;
 
-const choices = document.querySelectorAll('.choice');
-const result = document.querySelector('.result');
+gameStart();
 
-choices.forEach(choice => {
+choiceButtons.forEach(choice => {
     choice.addEventListener('click', () => {
         playRound(choice);
     })
+});
+
+rematchButton.addEventListener('click', () => {
+    resetGame();
 });
